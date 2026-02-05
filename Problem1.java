393ad82fe0c3f5599f498ddd54f0822e13f9a27b
Akshay <akshaymkgm98@gmail.com>
2026-02-04 23:40:59 -0500

    Submitting Q1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        int i = 0, j = 1;
+        int count =0 ;
+        int sum = Integer.MIN_VALUE;
+        Arrays.sort(nums);
 
+        while(j < nums.length && i<nums.length-1){
+            int diff = nums[j] - nums[i];
+
+            if(diff == k && sum != nums[i] + nums[j]){
+              sum = nums[i]+nums[j];
+              count++;
+              i++;
+              j++;
+            }
+            else if(diff < k) j++;
+            else i++;
+
+            if(i == j) j++;
+        }
+    return count;
+    }
+}
